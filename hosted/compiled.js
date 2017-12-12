5ea8e57da305ee6b2c452ae861787f2db9735980
siuster95 <jxs7205@rit.edu>
2017-12-12 14:01:50 -0500

    added shop option stuff

--- hosted/compiled.js
+++ hosted/compiled.js
@@ -417,6 +417,24 @@ var Room = function () {
 ;
 "use strict";
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var shopOption = function shopOption(x, y, width, height, text1, text2) {
+    _classCallCheck(this, shopOption);
+
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.text1 = text1;
+    this.text2 = text2;
+    this.text1positionX = this.x + this.width / 2;
+    this.text1positionY = this.y + 20;
+    this.text2positionX = this.x + this.width / 2;
+    this.text2positionY = this.y + this.height - 50;
+};
+"use strict";
+
 var particles = [];
 
 var rpcCall = function rpcCall() {
